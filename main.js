da8ba22701eadb22198e2c19f17c803cde2e375a
Jayson Boakye <jaysonboakye10@gmail.com>
2024-01-25 11:11:00 +0000

    add messenger func

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ let openerArr = ['What do we have here,', 'Hey, it looks like', 'Interesting res
 let middleArr = ['you have packed', 'from this pack you got', 'you may have pulled', 'you have great luck, it\'s', 'drumroll please...it\'s'];
 
 let endingArr = ['Mbappe out of the pool', 'Foden out of the pool', 'Haaland out of the pool', 'Bellingham out of the pool', 'Gullit out of the pool'];
+
+const futMessenger = () => `${openerArr[Math.floor(Math.random() * 8)]} ${middleArr[Math.floor(Math.random() * 5)]} ${endingArr[Math.floor(Math.random() * 5)]}`;
+console.log(futMessenger());
