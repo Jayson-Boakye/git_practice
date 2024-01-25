a9ae3f1336b16d7235f98fd3c2063f3ef6ca04ca
Jayson Boakye <jaysonboakye10@gmail.com>
2024-01-25 11:04:38 +0000

    add ending array

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 let openerArr = ['What do we have here,', 'Hey, it looks like', 'Interesting results,', 'One of the best out there,', 'Great pack pull,', 'Fortune favours you,', 'If your seeing this then,', 'The EA gods have answered your prayers,'];
 
 let middleArr = ['you have packed', 'from this pack you got', 'you may have pulled', 'you have great luck, it\'s', 'drumroll please...it\'s'];
+
+let endingArr = ['Mbappe out of the pool', 'Foden out of the pool', 'Haaland out of the pool', 'Bellingham out of the pool', 'Gullit out of the pool'];
