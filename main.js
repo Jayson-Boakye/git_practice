4ca32e4030d35a4ac8c8b0dd50e5b6912a869814
Jayson Boakye <jaysonboakye10@gmail.com>
2024-01-31 14:25:07 +0000

    shortened opening array

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let openerArr = ['What do we have here,', 'Hey, it looks like', 'Interesting results,', 'One of the best out there,', 'Great pack pull,', 'Fortune favours you,', 'If your seeing this then,', 'The EA gods have answered your prayers,'];
+let openerArr = ['What do we have here,', 'Hey, it looks like', 'Interesting results,', 'One of the best out there,', 'Great pack pull,',
+ 'Fortune favours you,', 'If your seeing this then,', 'The EA gods have answered your prayers,'];
 
 let middleArr = ['you have packed', 'from this pack you got', 'you may have pulled', 'you have great luck, it\'s', 'drumroll please...it\'s'];
 
